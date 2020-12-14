46194b05808f848f9f222e42d8875f9c3e4adb6d
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2020-12-14 17:00:41 +0000

    adding a table to Chinook

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,20 @@
 import os
+import datetime
 import pymysql
 
-# Get username from cloud 9 workspace
-# (modify this variable if running in other environment)
-username = os.getenv("C9_USER")
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
 
 # Connect to the database
-connection = pymysql.connect(host="localhost",
+connection = pymysql.connect(host='localhost',
                              user=username,
-                             password="",
-                             db="Chinook")
+                             password='',
+                             db='Chinook')
 
 try:
-    # Run a quary
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("Update Friends set age= 22 where name = 'Bob';")
+        connection.commit()
 finally:
-    # Close the connection, regardless of whether the above was successful
     connection.close()
